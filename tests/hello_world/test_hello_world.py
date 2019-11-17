24ff18d69cfb96ecc8b61a5c93c9aaf8e529ad4c
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2019-11-17 21:18:42 +0900

    Feat: add test pattern for using pypi package

--- tests/hello_world/test_hello_world.py
+++ tests/hello_world/test_hello_world.py
@@ -1,7 +1,12 @@
-from hello_world.hello_world import hello_world
+from hello_world.hello_world import hello_world, emojize
 
 
 def test_hello_world() -> None:
     name = 'Jhon'
     expected = 'Hello World {}.'.format(name)
     assert hello_world(name) == expected
+
+
+def test_emojize() -> None:
+    expected = 'Python is 👍'
+    assert emojize('Python is :thumbs_up:') == expected
